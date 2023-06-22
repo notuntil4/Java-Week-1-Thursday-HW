6ea4bf5ec1ef565a93ac9e96fec09d7d79dd10aa
Orquidia Moreno <orquidiamoreno26@gmail.com>
2023-06-22 14:51:48 -0400

    first commit

---
+++ src/com/cognixia/jump/ConvertTime.java
@@ -0,0 +1,15 @@
+package com.cognixia.jump;
+
+public class ConvertTime {
+	
+	public static void main(String[] args) {
+		
+		// Create Scanner and ask user for input (amount of seconds)...
+		
+		
+		// ...then print the time in hours, minutes, and seconds
+		
+		
+	}
+
+}
